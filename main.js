0a74df9ab64661a0c30b705c7e8a87e8a78e3ba1
MINJAE <minjae1543@gmail.com>
2025-02-17 14:17:49 +0900

    add new code

--- main.js
+++ main.js
@@ -3,22 +3,31 @@ let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
 let resetButton = document.getElementById("reset-button")
-let chances = 5
+let chances = 3
 let gameOver = false
 let chanceArea = document.getElementById("chance-area")
 let history = []
-
+let answer = document.getElementById("answer")
+let historyArea = document.getElementById("history-area")
 
 playButton.addEventListener("click", play)
 resetButton.addEventListener("click", reset)
 userInput.addEventListener("focus", function () {
     userInput.value = "";
 })
+answer.addEventListener("mouseenter", function () {
+    answer.textContent = `${computerNum}`; // 호버 시 정답 노출
+})
+answer.addEventListener("mouseleave", function () {
+    answer.textContent = "정답 확인(hover)"; // 마우스 떼면 다시 숨김
+});
+
 
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100)+1;
         console.log("정답", computerNum);
 }
+
 
 function play() {
     let userValue = userInput.value;
@@ -40,7 +49,7 @@ function play() {
     }
 
     chances--;
-    chanceArea.textContent = `CHANCE : ${chances}`
+    chanceArea.textContent = `LEFT CHANCE : ${chances} !!`
 
 
     if (userValue < computerNum) {
@@ -54,9 +63,15 @@ function play() {
 
     history.push(userValue);
     console.log(history);
+    historyArea.textContent = `history : ${history}`
 
     if (chances < 1) {
+        if(userValue !== computerNum){
         gameOver = true;
+        resultArea.textContent = "GAME OVER"}
+        if(userValue == computerNum){
+            gameOver = true;
+            resultArea.textContent = "BINGO!!"}
     }
 
     if (gameOver == true) {
@@ -69,14 +84,15 @@ function reset() {
     userInput.value = "";
     pickRandomNum();
 
-    chances = 5;
+    chances = 3;
     history = [];
     gameOver = false;
 
     resultArea.textContent = "UP OR DOWN"
-    chanceArea.textContent = `CHANCE : ${chances}`;
+    chanceArea.textContent = `CHANCE : ${chances} !!`;
+    historyArea.textContent = `history ${history}`
 
     playButton.disabled = false;
-    
+
 }
 pickRandomNum();
